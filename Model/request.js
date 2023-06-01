b57043183229c89cc0700e26c86b978bad281aba
André Dias <andre25dias@gmail.com>
2023-06-01 11:16:22 -0300

    testando variaveis

--- Model/request.js
+++ Model/request.js
@@ -1,1 +1,12 @@
+// Acesse a variável de ambiente da chave da API
 const apiKey = process.env.API_KEY;
+
+// Use a chave da API em suas chamadas para a API RAWG
+fetch(`https://api.rawg.io/games?key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // Manipule os dados da API aqui
+  })
+  .catch(error => {
+    // Trate erros aqui
+  });
