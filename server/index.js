962de60977b559d4721b9d2f4474fe8386111beb
André Dias <andre25dias@gmail.com>
2023-07-30 13:06:02 -0300

    testando serverless

--- server/index.js
+++ server/index.js
@@ -1,30 +1,24 @@
-// server/index.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 // Permitir todas as origens (não recomendado em produção)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-  });
-  
-  // Sua rota para buscar a chave de API
-  app.get('/api', (req, res) => {
+});
+
+// Sua rota para buscar a chave de API
+app.get('/api', (req, res) => {
     const apiKey = process.env.API_KEY;
     if (apiKey) {
-      res.json({ apiKey });
+        res.json({ apiKey });
     } else {
-      res.status(500).json({ message: 'API key not found.' });
+        res.status(500).json({ message: 'API key not found.' });
     }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+});
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
