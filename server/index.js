f1c161b4cfe9e3a90cbd1fdb841bd6b3cf31e255
André Dias <andre25dias@gmail.com>
2023-07-30 13:30:09 -0300

    testando problema da api

--- server/index.js
+++ server/index.js
@@ -3,15 +3,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Permitir todas as origens (não recomendado em produção)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
+
+app.use('/api', (req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
 });
 
 // Sua rota para buscar a chave de API
 app.get('/api', (req, res) => {
     const apiKey = process.env.API_KEY;
+    console.log("API Key:", apiKey);
     if (apiKey) {
         res.json({ apiKey });
     } else {
